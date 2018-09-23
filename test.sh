ae06fa62547f1dc0e3076b60dfbb36c4c0c09f97
Victor Peng <victor.peng@airbnb.com>
2018-09-22 17:19:38 -0700

    test german language (non-ASCII)

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-curl 'http://localhost:8080/geocode?address=888+Brannan+Street+San+Francisco'
+curl -v 'http://localhost:8080/geocode?address=888+Brannan+Street+San+Francisco'
+
+curl -v "http://localhost:8080/geocode?address=Rathausstra%C3%9Fe%2015,%2010178%20Berlin,%20Germany"
+
