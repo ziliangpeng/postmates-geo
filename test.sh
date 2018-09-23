fad5822986979f1277834ae4e6cad5ee97f4988a
Victor Peng <victor.peng@airbnb.com>
2018-09-22 17:56:15 -0700

    add fake address to test

--- test.sh
+++ test.sh
@@ -4,3 +4,5 @@ curl -v 'http://localhost:8080/geocode?address=888+Brannan+Street+San+Francisco'
 
 curl -v "http://localhost:8080/geocode?address=Rathausstra%C3%9Fe%2015,%2010178%20Berlin,%20Germany"
 
+curl -v "http://localhost:8080/geocode?address=Somewhere+Only+We+Know"
+
